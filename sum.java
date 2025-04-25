da32186252ad700740db3761652beca624b4d984
akash5800 <ap58029900@gmail.com>
2025-04-25 22:27:04 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+        int sum = num1 + num2; // Calculate sum
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
